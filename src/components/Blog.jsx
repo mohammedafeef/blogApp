ce1e74b73e17207c663ddfea4ab76953c26d2bc2
mohammedafeef <mohammedafeef.maf@gmail.com>
2021-02-22 00:26:57 +0530

    blog component styling is done

---
+++ src/components/Blog.jsx
@@ -0,0 +1,18 @@
+const Blog = ()=>{
+    return(
+        <section className="blog-out">
+            <div className="tittle">
+                <h1></h1>
+            </div>
+            <div className="content">
+                <p></p>
+            </div>
+            <div className="expand">
+                <button className="exp-button">
+                    continue reading
+                </button>
+            </div>
+        </section>
+    )
+}
+export default Blog;
--- src/components/blogs/components/BlogFullView.jsx
+++ src/components/blogs/components/BlogFullView.jsx
@@ -2,24 +2,32 @@ const BlogFullView =()=>{
     return(
         <section className="blog-in">
             <div className="headbar">
-                <h1>
-
-                </h1>
+                <div className="icon-back">
+                    
+                </div>
                 <div className="icon-dlt">
 
                 </div>
             </div>
             <div className="content">
+                <h1>
+
+                </h1>
                 <h3>
 
                 </h3>
                 <p>
 
                 </p>
-                <span>
-                    
-                </span>
+                <hr/>
+            </div>
+            <div className="social-links">
+                    <button className="social-btn"><a href="#"/></button>
+                    <button className="social-btn"><a href="#"/></button>
+                    <button className="social-btn"><a href="#"/></button>
+                    <button className="social-btn"><a href="#"/></button>j
             </div>
         </section>
     )
 }
+export default BlogFullView;
